8e4bfbc80dacb5e34011f28e8735c33c23f4a92e
Md.Rabiul Hasan <70057377+md-rabiul-hasan@users.noreply.github.com>
2025-01-13 15:17:16 +0600

    common exception added

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,10 +3,15 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Illuminate\Http\Exceptions\ThrottleRequestsException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
@@ -14,5 +19,34 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+
+        $exceptions->render(function (NotFoundHttpException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'success'     => false,
+                    'status_code' => 404,
+                    'message'     => 'Route Not found',
+                ], 404);
+            }
+        });
+
+        $exceptions->render(function (MethodNotAllowedHttpException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'success'     => false,
+                    'status_code' => 405,
+                    'message'     => 'The HTTP method used is not allowed for this route.'
+                ], 405);
+            }
+        });
+
+        $exceptions->render(function (ThrottleRequestsException $e, Request $request) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Too many requests. Please slow down.',
+                'status_code' => 429,
+            ], 429);
+        });
+
+
     })->create();
---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::group(['prefix' => 'v1'], function(){
+    Route::get('hello', function(){
+        return "hello world";
+    });
+});
